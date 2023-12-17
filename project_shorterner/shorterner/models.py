d41c286c20d5b64ae4a6cedc98c3217b06d6a106
german <nextphasesoon@mail.ru>
2023-12-18 00:32:56 +0300

    start

--- project_shorterner/shorterner/models.py
+++ project_shorterner/shorterner/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
+from django.contrib.auth.models import User, AbstractUser
+
 
 
 class ShortURL(models.Model):
@@ -13,8 +12,6 @@ class ShortURL(models.Model):
     def __str__(self) -> str:
         return self.original_url
     
-# @receiver(pre_save, sender=ShortURL)
-# def set_user_to_url(sender, instance, *args, **kwargs): #sender - модель, которая отправляет, instance - экземляр модели, модели, которая вызвала, create - была ли добавлена новая запись в бд(true, false)
-#     if
-#         if not instance.pk:
-#         instance.url_owner = instance.url_owner
+
+class User(AbstractUser):
+    pass
