52ae3499508c03205412fc545190f43aedb55e4b
german <nextphasesoon@mail.ru>
2023-12-19 00:55:11 +0300

    basic' e-mail verify done

--- project_shorterner/project_shorterner/urls.py
+++ project_shorterner/project_shorterner/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as auth_views
 from shorterner.views import *
 
 urlpatterns = [
+    path('login/', user_views.LoginViewCustom.as_view(), name='login'),
     path("password_reset/", auth_views.PasswordResetView.as_view(template_name='reset_password/password_reset_form.html'), name="password_reset"),
     path("password_reset/done/",auth_views.PasswordResetDoneView.as_view(template_name='reset_password/password_done.html'),name="password_reset_done"),
     path("reset/<uidb64>/<token>/",auth_views.PasswordResetConfirmView.as_view(template_name='reset_password/password_confirm.html'),name="password_reset_confirm"),
--- project_shorterner/shorterner/models.py
+++ project_shorterner/shorterner/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
 
 
@@ -7,11 +8,14 @@ class ShortURL(models.Model):
     original_url = models.CharField(max_length=700)
     short_url = models.CharField(max_length=100)
     created_at = models.DateTimeField(null=True)
-    url_owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    url_owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self) -> str:
         return self.original_url
     
 
 class User(AbstractUser):
-    pass
+    is_email_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
